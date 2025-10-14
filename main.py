4cd3055affac8ae0a7c6cff479e4851ddc85e3b5
declanmerry <delcan.merry@gmail.com>
2025-10-14 22:30:05 +0100

    Adding origins updates

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ app = FastAPI()
 # Allow your frontend to access this API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    #"https://weather-dashboard-frontend-tau.vercel.app",
-    #"https://weather-dashboard-frontend-8qseputtt-declanmerrys-projects.vercel.app"
+    allow_origins=[
+        "https://weather-dashboard-frontend-bwj93bdrn-declanmerrys-projects.vercel.app",
+        "https://weather-dashboard-frontend-tau.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
